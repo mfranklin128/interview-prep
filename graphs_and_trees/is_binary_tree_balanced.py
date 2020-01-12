2806dfc75907a05e44aabbc47e94f8864ad598f7
EC2 Default User <ec2-user@ip-172-31-31-33.us-west-2.compute.internal>
2020-01-12 22:52:42 +0000

    implement balanced tree, ctci 4.1

---
+++ graphs_and_trees/is_binary_tree_balanced.py
@@ -0,0 +1,42 @@
+import binary_tree
+
+# 4
+# |\
+# 2 1
+# |\
+# 6 1 
+# |
+# 7
+def construct_test_data():
+    root = binary_tree.Node(4)
+    root.left = binary_tree.Node(2)
+    root.left.left = binary_tree.Node(6)
+    root.left.left.left = binary_tree.Node(7)
+    root.right = binary_tree.Node(1)
+    root.left.right = binary_tree.Node(1)
+    return root
+    
+    
+import sys
+
+def balanced_depth(root):
+    if not root:
+        return 0
+    print(root.data)
+    left_depth = balanced_depth(root.left)
+    right_depth = balanced_depth(root.right)
+    if abs(left_depth - right_depth) > 1:
+        print('diff > 1')
+        return -1
+    if left_depth == -1 and right_depth == -1:
+        print('both unbalanced')
+        return -1
+    print('depth = %d' % (max(left_depth, right_depth) + 1))
+    return max(left_depth, right_depth) + 1
+
+def is_balanced(root):
+    return balanced_depth(root) != -1
+        
+
+tree = construct_test_data()
+print(is_balanced(tree))
