0f6ce1315b554c3f2614421ba86b525fe56bba63
EC2 Default User <ec2-user@ip-172-31-31-33.us-west-2.compute.internal>
2020-01-15 02:21:24 +0000

    fix is bst impl

--- graphs_and_trees/is_binary_search_tree.py
+++ graphs_and_trees/is_binary_search_tree.py
@@ -7,27 +7,32 @@ def make_testdata():
     root.left.left.left = binary_tree.Node(0)
     root.left.left.right = binary_tree.Node(3)
     root.right = binary_tree.Node(8)
-    root.right.left = binary_tree.Node(6)
-    root.right.right = binary_tree.Node(9)
-    root.right.right.left = binary_tree.Node(4)
-    root.right.right.right = binary_tree.Node(10)
+    root.right.left = binary_tree.Node(7)
+    root.right.right = binary_tree.Node(12)
+    root.right.right.left = binary_tree.Node(9)
+    root.right.right.right = binary_tree.Node(11)
     return root
     
-def is_bst(root):
-    if not root:
-        return True
+def is_bst_helper(root):
     if not root.left and not root.right:
-        return True
-    if not root.left:
-        return root.right.data > root.data and is_bst(root.right)
-    if not root.right:
-        return root.left.data < root.data and is_bst(root.left)
-    return (
-        root.left.data < root.data and
-        root.right.data > root.data and
-        is_bst(root.left) and
-        is_bst(root.right)
-    )
+        return root.data
+    left_max = None
+    right_max = None
+    if root.left:
+        left_max = is_bst_helper(root.left)
+        if left_max == None or left_max > root.data:
+            return None
+    if root.right:
+        right_max = is_bst_helper(root.right)
+        if right_max == None or right_max < root.data:
+            return None
+    if right_max:
+        return right_max
+    return root.data
+        
+    
+def is_bst(root):
+    return is_bst_helper(root) != None
 
 root = make_testdata()
 print(is_bst(root))
